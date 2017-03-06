04b0a03ec4f695fb6d68a6be8eaaef60aaa72edd
whoami <whose@mail.ai>
2017-03-06 16:07:47 +0900

    grade up installation

--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,11 @@
 # vi: set ft=ruby :
 
 Vagrant.configure('2') do |config|
-  config.vm.box = 'bento/ubuntu-16.04'
+  config.vm.box = 'ubuntu/trusty64'
   config.vm.hostname = 'himeji'
-  config.vm.synced_folder '~/Downloads/', '/home/vagrant/Downloads'
-  config.vm.synced_folder '~/.ssh/', '/home/vagrant/.ssh'
+  config.vm.synced_folder '~/Downloads', '/home/vagrant/Downloads'
+  config.vm.synced_folder '~/Dropbox', '/home/vagrant/Dropbox'
+  config.vm.synced_folder '~/.dots', '/home/vagrant/.dots'
   config.ssh.forward_x11 = true
   config.vm.provision :shell, path: 'provision.sh'
   config.vm.provider :virtualbox do |vb|
--- provision.sh
+++ provision.sh
@@ -1,5 +1,51 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt install -y automake g++ git ruby zsh
-sudo apt install -y vim screen pdftk
+# configure
+cat <<EOM > /home/vagrant/.vimrc
+source ~/.dots/vimrc
+source ~/.dots/vim/appearance.dark.vim
+EOM
+
+cat <<EOM > /home/vagrant/.zshrc
+source ~/.dots/zshrc
+export PATH=~/bin:$PATH
+export PATH=~/.nodebrew/current/bin:$PATH
+EOM
+
+cat <<EOM > /home/vagrant/.screenrc
+source ~/.dots/screenrc
+source ~/.dots/screen/dark
+EOM
+
+# apt
+apt-get update -y
+apt-get install -y apt-file automake build-essential g++ git graphviz imagemagick pandoc pdftk rlwrap ruby vim zsh jq unzip libncurses5-dev libncursesw5-dev texinfo
+apt-get clean
+apt-file update
+
+# manual install
+git clone https://github.com/cympfh/bin.git /home/vagrant/bin
+
+cd ~
+curl -L git.io/nodebrew | perl - setup
+mv .nodebrew /home/vagrant/
+
+mkdir /home/vagrant/Tools
+cd /home/vagrant/Tools
+wget -O vim-master.zip https://github.com/vim/vim/archive/master.zip
+unzip vim-master.zip
+mv vim-master vim
+cd vim
+./configure && make && make install
+mkdir /home/vagrant/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git /home/vagrant/.vim/Vundle.vim
+
+mkdir /home/vagrant/Tools
+cd /home/vagrant/Tools
+wget -O screen.tar.gz http://ftp.gnu.org/gnu/screen/screen-4.5.0.tar.gz
+tar xf screen.tar.gz
+cd screen-*
+./configure && make && make install
+
+# post-processing
+chown -R vagrant:vagrant /home/vagrant/
